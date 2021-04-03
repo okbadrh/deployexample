ee39c8d307561967588dd405b782e64be704e2ea
okba <ookbaa@hotmail.fr>
2021-04-03 12:53:54 +0100

    added .env

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const path = require("path")
+require("dotenv").config()
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, "./public")
 app.use(express.static(publicDirectoryPath))
 
-const port = 3001
+const port = process.env.PORT
 
 app.listen(port, () =>{
     console.log(`Server is up on port ${port}`)
